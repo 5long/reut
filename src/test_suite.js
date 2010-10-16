16673e7bfa87bed0732be96b06afaaf62a855e49
Whyme.Lyu <5longluna@gmail.com>
2010-10-17 01:43:32 +0800

    TestSuite#run can report now, really.

--- src/test_suite.js
+++ src/test_suite.js
@@ -4,14 +4,20 @@ var util = require('./util')
 function TestSuite(desc) {
   this.desc = desc
   this._testCases = []
+  this._results = []
 }
 util.inherits(TestSuite, EventEmitter)
 
 util.merge(TestSuite.prototype, {
   run: function(cb) {
+    var thisSuite = this
     var actions = this._testCases.map(function(tc) {
-      return function() { tc.run(this) }
-    })
+          return function() {
+            tc.run(function(err, result) {
+              this(err, {desc: tc.desc, result: result})
+            }.bind(this))
+          }
+        })
     util.serial(actions, cb)
   }
 , add: function(testCase) {
--- test/test_suite_normal.js
+++ test/test_suite_normal.js
@@ -3,9 +3,10 @@ var reut = require('../src')
   , TestCase = reut.TestCase
   , assert = require('assert')
   , holder = {status: 1}
+  , testCaseDesc = "a test case"
 
 var aSuite = new TestSuite("no desc")
-aSuite.add(new TestCase("a test case", function(test) {
+aSuite.add(new TestCase(testCaseDesc, function(test) {
   setTimeout(function() {
     test.ok(1, "I'm done")
     test.end()
@@ -16,6 +17,8 @@ aSuite.run(function(err, report) {
   assert.ifError(err)
   holder.status = 0
   assert.equal(report.length, 1)
+  assert.equal(report[0].desc, testCaseDesc)
+  assert.equal(report[0].result.passed.length, 1)
 })
 
 assert.ok(holder.status)
