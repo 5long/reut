d64479d733029ddec2c11e310c56d2d6eb8bdd80
Whyme.Lyu <5longluna@gmail.com>
2010-10-17 16:32:13 +0800

    Ad-hoc test reporter.

--- src/runner.js
+++ src/runner.js
@@ -1,8 +1,11 @@
 var TestSuite = require("./test_suite")
   , TestCase = require("./test_case")
   , util = require("./util")
+  , sys = require("sys")
   , suites = []
+  , pushAll = Function.prototype.apply.bind(Array.prototype.push)
 
+// TODO Extract the test runner interface.
 var runner = module.exports = {
   suite: function(desc) {
     suites.push(new TestSuite(desc))
@@ -13,11 +16,36 @@ var runner = module.exports = {
     var suite = suites[suites.length - 1]
     suite.add(new TestCase(desc, action))
   }
+, run: function(cb) {
+    if (typeof cb != "function") cb = util.noop
+    var actions = suites.map(function(suite) {
+      return function() { suite.run(this) }
+    })
+    util.serial(actions, cb)
+  }
 }
 
 process.nextTick(function() {
-  var actions = suites.map(function(suite) {
-    return function() { suite.run(this) }
+  // TODO Extract the following code as a standalone reporter
+  runner.run(function(err, results) {
+    var flatterned = results.reduce(function F(flatterned, result) {
+          return flatterned.concat(
+              result instanceof Array
+            ? result.reduce(F, [])
+            : [result]
+            )
+        }, [])
+      , report = flatterned.reduce(function R(total, result) {
+          result = result.result
+          pushAll(total.all, result.all)
+          pushAll(total.failed, result.failed)
+          pushAll(total.passed, result.passed)
+          return total
+        }, {all:[], passed:[], failed:[]})
+      , errorMsg = report.failed.map(function(err) {
+          return err.stack || err.message
+        }).join("\n")
+    sys.print(errorMsg)
+    console.log("%d assertions, %d failed.", report.all.length, report.failed.length)
   })
-  util.serial(actions)
 })
