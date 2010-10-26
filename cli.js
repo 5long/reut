15cc5b5046fbd5ff4124d9c554bfe08380d88396
Whyme.Lyu <5longluna@gmail.com>
2010-10-26 16:19:55 +0800

    cli.js provides reporter.

--- cli.js
+++ cli.js
@@ -3,6 +3,10 @@ var runner = require("./src/runner")
   , util = require("./src/util")
   , async = util.async
   , path = require("path")
+  , reporterMod = require("./src/reporter")
+  , reporters = [ new reporterMod.Simple()
+                , new reporterMod.Summary()
+                ]
 
 function main() {
   var files = util.makeArray(process.argv, 2)
@@ -12,7 +16,7 @@ function main() {
     loadAndRun(path.join(cwd, file), this)
   }, function(err) {
     if (err) throw err
-    runner.run(function(err) {
+    runner.run({reporters:reporters}, function(err) {
       if (err) throw err
     })
   })
