809dec16a94651ce2169ed8462ec147b261e476d
Whyme.Lyu <5longluna@gmail.com>
2010-10-26 16:11:42 +0800

    Example for assertion extension

---
+++ example/extended_assert.js
@@ -0,0 +1,12 @@
+var reut = require("reut")
+
+reut.suite("Some more assertion method")
+
+var tc = reut.test("Cuz CommonJS is slow at evolving", function(assert) {
+  assert.include("foo", "oo", "Wraps indexOf != -1")
+  assert.length([], 1, "This should fail")
+  assert.instanceOf({}, Object)
+  assert.typeOf({}, "number", "This should fail")
+  assert.match("blah", /ah/)
+  assert.end()
+})
