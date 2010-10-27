71114923d19195c47e7f06744759326dbcf66451
Whyme.Lyu <5longluna@gmail.com>
2010-10-28 00:24:10 +0800

    util.def

--- src/util.js
+++ src/util.js
@@ -17,6 +17,14 @@ var util = module.exports = {
 , inherits: function(klass, super) {
     klass.prototype = Object.create(super.prototype)
   }
+, def: function(dest, source) {
+    if (arguments.length < 2) throw TypeError("Wrong number of arguments")
+    var props = Object.getOwnPropertyNames(source)
+    props.forEach(function(prop) {
+      var pd = Object.getOwnPropertyDescriptor(source, prop)
+      Object.defineProperty(this, prop, pd)
+    }, dest)
+  }
 , noop: noop
 }
 
